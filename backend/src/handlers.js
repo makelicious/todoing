cbab2fbbb494b27cae367a076c4520636ca6ba30
makelicious <markus.laurila@futurice.com>
2017-12-17 15:39:47 +0200

    remove redundant destructuring

--- backend/src/handlers.js
+++ backend/src/handlers.js
@@ -6,18 +6,9 @@ const { raw } = connection;
 
 function postIdea(request, h) {
   const { text, type } = request.payload;
-  const { what, when, why, how, done } = type;
-  const postgresParams = {
-    text,
-    done,
-    what,
-    when,
-    why,
-    how,
-  };
 
   raw(`INSERT INTO ideas (created_at, updated_at, text, done, what, "when", why, how)
-      VALUES (now(), NULL, :text, :done, :what, :when, :why, :how)`, postgresParams)
+      VALUES (now(), NULL, :text, :done, :what, :when, :why, :how)`, { text, type })
     .catch(() => h.response('Server error').code(503));
 
   return h.response('coolio').code(201);
