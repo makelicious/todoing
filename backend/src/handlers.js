85bded568aab75c634575ba499d2bc57cc975023
makelicious <markus.laurila@futurice.com>
2017-12-26 15:48:12 +0200

    Store only distinct hashtags

--- backend/src/handlers.js
+++ backend/src/handlers.js
@@ -3,6 +3,7 @@ const knexConfig = require('../knexfile');
 const connection = knex(knexConfig);
 const { raw, transaction } = connection;
 const uuidv4 = require('uuid/v4');
+const _ = require('lodash');
 
 
 async function postIdea(request, h) {
@@ -15,16 +16,20 @@ async function postIdea(request, h) {
       VALUES (:ideaId, now(), NULL, :text, :done, :what, :when, :why, :how) RETURNING *`, { ...type, text, ideaId })
 
       if (tags.length > 0) {
-        const queries = tags.reduce((queries, tag, i) => ({
-          tagQuery: queries.tagQuery.concat(` ('${uuidv4()}', now(), NULL, '${tag}') ${i === tags.length - 1 ? '' : ','}`),
-          ideasTagQuery: queries.ideasTagQuery.concat(` ('${ideaId}', '${tag}') ${i === tags.length - 1 ? '' : ','}`),
-        }), {
-            tagQuery: 'INSERT INTO tags (id, created_at, updated_at, name) VALUES',
-            ideasTagQuery: 'INSERT INTO ideas_tags (idea_id, tag_name) VALUES',
-          });
+        const distinctTags = await filterDuplicateTags(tags);
 
-        await trx.raw(queries.tagQuery);
-        await trx.raw(queries.ideasTagQuery);
+        if (distinctTags.length > 0) {
+          const insertQueries = distinctTags.reduce((queries, tag, i) => ({
+            tags: queries.tags.concat(` ('${uuidv4()}', now(), NULL, '${tag}') ${i === distinctTags.length - 1 ? '' : ','}`),
+            ideasTags: queries.ideasTags.concat(` ('${ideaId}', '${tag}') ${i === distinctTags.length - 1 ? '' : ','}`),
+          }), {
+              tags: 'INSERT INTO tags (id, created_at, updated_at, name) VALUES',
+              ideasTags: 'INSERT INTO ideas_tags (idea_id, tag_name) VALUES',
+            });
+
+          await trx.raw(insertQueries.tags);
+          await trx.raw(insertQueries.ideasTags);
+        }
       }
 
       return h.response(formatIdea(insertedIdea.rows[0])).code(201);
@@ -43,6 +48,19 @@ async function getIdeas(request, h) {
   catch (err) {
     return h.response('Cant get ideas').code(500);
   }
+}
+
+async function filterDuplicateTags(tags) {
+  const tagSelectQuery = tags.reduce((query, tag, i) =>
+    query.concat(` name = '${tag}' ${i === tags.length - 1 ? '' : 'OR'}`)
+    , 'SELECT name FROM tags WHERE');
+
+  const duplicateTags = await raw(tagSelectQuery);
+  const duplicateTagNames = duplicateTags.rows.map((tag) => tag.name);
+
+  return duplicateTags.rows.length === 0
+    ? tags
+    : _.reject(tags, (tag) => _.includes(duplicateTagNames, tag));
 }
 
 
