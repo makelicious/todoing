e1f624dd3fcf627f7f1a0cbea3877c22d2fa87a0
makelicious <markus.laurila@futurice.com>
2017-12-28 04:22:30 +0200

    make filter great again

--- frontend/src/modules/filters.js
+++ frontend/src/modules/filters.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 const FILTER_IDEAS_BY_TAG = 'FILTER_IDEAS_BY_TAG';
 const FILTER_IDEAS_BY_TYPE = 'FILTER_IDEAS_BY_TYPE';
 const RESET_FILTERS = 'RESET_FILTERS';
@@ -26,7 +28,7 @@ export default function reducer(state = initialState, action = {}) {
     case FILTER_IDEAS_BY_TAG:
       return {
         ...state,
-        tags: state.tags.concat(action.tag),
+        tags: updateFilterTags(state.tags, action.tag),
       };
     case RESET_FILTERS:
       return state;
@@ -46,4 +48,10 @@ export function filterByTag(tag) {
     type: FILTER_IDEAS_BY_TAG,
     tag,
   };
+}
+
+function updateFilterTags(oldTags, newTag) {
+  return oldTags.length > 0
+    ? _.reject(oldTags, (tag => tag === newTag))
+    : [newTag];
 }
